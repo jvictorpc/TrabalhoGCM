4e39eef63f6f343586051d6856aa68f3c156a4fb
jvictorpc <34607628+jvictorpc@users.noreply.github.com>
2018-04-06 18:38:01 -0300

    Mudança das perguntas
    agora o assunto é cultura, taambém add a biblioteca locale pra poder escrever em português

--- Questionario.c
+++ Questionario.c
@@ -5,21 +5,25 @@
 int main(){
 	char resposta1[141], resposta2[141], resposta3[141], resposta4[141], resposta5[141];
 	setlocale(LC_ALL,"portuguese");
-	printf("Qual seu nome?\n");
-	scanf("%s",&resposta1);
+	printf("QUESTIONÁRIO CINÉFILO\n");
+	printf("__________________________________________________\n");
+
+	printf("\nQuem dirigiu 2001: Uma odisséia no espaço?");
+	scanf("%s", &res1[141]);
 	system("cls");
-	printf("Qual a sua idade?\n");
-	scanf("%s", &resposta2);
-	system("cls");
-	printf("Você gosta de vacas?\n");
-	scanf("%s", &resposta3);
-	system("cls");
-	printf("Qual o seu tipo sanguineo?\n");
-	scanf("%s", &resposta4);
-	system("cls");
-	printf("Qual seu tipo de musica favorito?\n");
-	scanf("%s", &resposta5);
-	system("cls");
+	printf("\nQuantas cenas improvisadas tiveram em senhor dos anéis? ");
+	scanf("%s", &res2[141]);
+	system("cls");	
+	printf("\nEscreva o nome do melhor amigo de Han Solo, sem pesquisar no google:");
+	scanf("%s", &res3[141]);
+	system("cls");	
+	printf("\nQuantos anos boyhood demorou pra ser filmado?");
+	scanf("%s", &res4[141]);
+	system("cls");	
+	printf("\nQual o melhor filme do Adam Sandler? ");
+	scanf("%s", &res5[141]);
+	system("cls");	
+	
 	
 	return 0;
 }
