2ca412662572ef9f0f22dc503aacd8359f883da4
jvictorpc <34607628+jvictorpc@users.noreply.github.com>
2018-04-06 19:20:00 -0300

    Update Questionario.c

--- Questionario.c
+++ Questionario.c
@@ -9,19 +9,19 @@ int main(){
 	printf("__________________________________________________\n");
 
 	printf("\nQuem dirigiu 2001: Uma odisséia no espaço?");
-	scanf("%s", &res1[141]);
+	scanf("%s", &res1[40]);
 	system("cls");
 	printf("\nQuantas cenas improvisadas tiveram em senhor dos anéis? ");
-	scanf("%s", &res2[141]);
+	scanf("%s", &res2[40]);
 	system("cls");	
 	printf("\nEscreva o nome do melhor amigo de Han Solo, sem pesquisar no google:");
-	scanf("%s", &res3[141]);
+	scanf("%s", &res3[40]);
 	system("cls");	
 	printf("\nQuantos anos boyhood demorou pra ser filmado?");
-	scanf("%s", &res4[141]);
+	scanf("%s", &res4[40]);
 	system("cls");	
 	printf("\nQual o melhor filme do Adam Sandler? ");
-	scanf("%s", &res5[141]);
+	scanf("%s", &res5[40]);
 	system("cls");	
 	printf("CONFIRA SUAS RESPOSTAS:\n");
 	printf("_________________________________________________________\n");
