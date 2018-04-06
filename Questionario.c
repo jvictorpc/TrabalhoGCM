58126ae002f4a28b12ecb3d8d3acc93676a796a5
EustakioJr <eustakio.jr@gmail.com>
2018-04-06 18:50:43 -0300

    Respostas
    Adição de respostas para comparar

--- Questionario.c
+++ Questionario.c
@@ -3,7 +3,7 @@
 #include <locale.h>
 
 int main(){
-	char resposta1[141], resposta2[141], resposta3[141], resposta4[141], resposta5[141];
+	char res1[141], res2[141], res3[141], res4[141], res5[141];
 	setlocale(LC_ALL,"portuguese");
 	printf("QUESTIONÁRIO CINÉFILO\n");
 	printf("__________________________________________________\n");
@@ -23,7 +23,9 @@ int main(){
 	printf("\nQual o melhor filme do Adam Sandler? ");
 	scanf("%s", &res5[141]);
 	system("cls");	
-	
+	printf("As respostas corretas são:");
+	printf("\nPergunta 1: Stanley Kubrick;\nPergunta 2: ;\nPergunta 3: Chewbacca;\nPergunta 4: 12 anos; \nPergunta 5: Punch Drunk Love;");
+	printf("\nPergunta 1: %s;\nPergunta 2: %s;\nPergunta 3: %s;\nPergunta 4: %s; \nPergunta 5: %s;", &res1, &res2, &res3, &res4, &res5);
 	
 	return 0;
 }
