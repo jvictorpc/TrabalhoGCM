42d00a4e5529ac573cd5ecaae3299ae3c0674409
EustakioJr <eustakio.jr@gmail.com>
2018-04-06 18:12:17 -0300

    Questionario 1

---
+++ Questionario.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char resposta1[141], resposta2[141], resposta3[141], resposta4[141], resposta5[141];
+	printf("Qual seu nome?\n");
+	scanf("%s",&resposta1);
+	printf("Qual a sua idade?\n");
+	scanf("%s", &resposta2);
+	printf("Você gosta de vacas?\n");
+	scanf("%s", &resposta3);
+	printf("\n");
+	return 0;
+}
